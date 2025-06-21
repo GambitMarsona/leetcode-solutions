bd0b81289cb81b06b95452aa94c8dd97eaaf38a0
Marek Polit <marekpolit21@email.com>
2025-06-21 12:40:48 +0200

    new solution remove elements from linked list (leetcode 203)

--- python/0083-remove-duplicates-from-sorted-list.py
+++ python/0083-remove-duplicates-from-sorted-list.py
@@ -10,8 +10,4 @@ class Solution:
             while curr.next and curr.val == curr.next.val:
                 curr.next = curr.next.next
             curr = curr.next
-        return head
-            
-        
-        
-        
+        return head  
